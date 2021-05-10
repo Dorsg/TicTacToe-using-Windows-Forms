b2d35fcabfcf1b687b98250d3d11079e357542ef
AvivBk <avivbarkai3@gmail.com>
2021-05-10 12:53:27 +0300

    11

--- TicTacToe/Board.cs
+++ TicTacToe/Board.cs
@@ -9,7 +9,7 @@ namespace TicTacToe
 {
     public class Board
     {
-        public  readonly char X = (char)88; // need to check how the whole project knows them
+        public  readonly char X = (char)88;
         public  readonly char O = (char)79;
 
         private char[,] m_GameBoard;
@@ -17,10 +17,9 @@ namespace TicTacToe
         public Board(int i_size)
         {
             r_BoardSize = i_size;
-            m_GameBoard = new char[i_size, i_size];
-            Init();
+            this.m_GameBoard = new char[i_size, i_size];
+            this.Init();
         }
-
         public void Init()
         {
             char space = (char)32;
@@ -29,39 +28,35 @@ namespace TicTacToe
             {
                 for (int j = 0; j < r_BoardSize; j++)
                 {
-                    m_GameBoard.SetValue(space, i, j);
+                    this.m_GameBoard.SetValue(space, i, j);
                 }
             }
         }
         public char this[int i_Idx1, int i_Idx2] 
         {
-            get { return m_GameBoard[i_Idx1 - 1, i_Idx2 - 1]; } // throw exception
+            get { return this.m_GameBoard[i_Idx1 - 1, i_Idx2 - 1]; }
             set { m_GameBoard.SetValue(value, i_Idx1 - 1, i_Idx2 - 1); }
         }
-
         public int Size
         {
             get { return r_BoardSize; }
         }
-
-        public  bool isSpotAvialable(int i_row, int i_col) // should throw exceptions in setter !!
+        public  bool isSpotAvialable(int i_Row, int i_Col)
         {
             bool flag = true;
-            if (i_row - 1 < 0 || i_row - 1 >= r_BoardSize) //wrong logic
+            if (i_Row - 1 < 0 || i_Row - 1 >= r_BoardSize)
             {
                 flag = false;
             }
-            else if (i_col - 1 < 0 || i_col - 1 >= r_BoardSize)
+            else if (i_Col - 1 < 0 || i_Col - 1 >= r_BoardSize)
             {
                 flag = false;
             }
-            else if (this[i_row, i_col] == X || this[i_row, i_col] == O)
+            else if (this[i_Row, i_Col] == X || this[i_Row, i_Col] == O)
             {
                 flag = false;
             }
             return flag;
         }
     }
-
-
 }
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,11 +1,7 @@
 ﻿namespace TicTacToe
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
+ 
     public class Program
     {
         public static void Main(string[] arg)
